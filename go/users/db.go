414f758488a47b334119e11d0acfa5ec7c818760
Zane Rockenbaugh <zane@liquid-labs.com>
2019-08-19 03:03:48 -0500

    use new update protocol

--- go/users/db.go
+++ go/users/db.go
@@ -7,6 +7,13 @@ import (
   . "github.com/Liquid-Labs/lc-entities-model/go/entities"
 )
 
+var UserFields = append(EntityFields,
+  `auth_id`,
+  `legal_id`,
+  `legal_id_type`,
+  `active`,
+)
+
 // Create creates (or inserts) a new User record into the DB. As Users are logically abstract, one would typically only call this as part of another items create sequence.
 func (u *User) CreateRaw(db orm.DB) Terror {
   if err := CreateEntityRaw(u, db); err != nil {
@@ -31,32 +38,21 @@ func init() {
   copy(updateExcludes, EntityFields)
   updateExcludes = append(updateExcludes, "id")
 }
+
+func (u *User) UpdateRawQueries(db orm.DB) []*orm.Query {
+  qs := (&u.Entity).UpdateRawQueries(db)
+  // No need for a Subjects query, there's nothing there. And there's a trivial bug (?) in go-pg (v8.0.5) where if you exclude all the fields (as we would) then it's the same as excluding none of the fields.
+  qu := db.Model(u).
+    ExcludeColumn(updateExcludes...).
+    Where(`"user".id=?id`)
+  qu.GetModel().Table().SoftDeleteField = nil
+
+  return append(qs, qu)
+}
+
 // Updates a User record in the DB. As Users are logically abstract, one would typically only call this as part of another items update sequence.
 func (u *User) UpdateRaw(db orm.DB) Terror {
-  if err := (&u.Subject.Entity).UpdateRaw(db); err != nil {
-    return err
-  } else {
-    /* So, there's really nothing on 'subjects' to update and when all columns
-       are excluded, go-pg treats it the same as if nothing was excluded, which
-       leads to exceptions when it tries to update the entity fields.
-    qs := db.Model((&u.Subject)).
-      ExcludeColumn(updateExcludes...).
-      Where(`subject.id=?id`)
-    qs.GetModel().Table().SoftDeleteField = nil
-    if _, err := qs.Update(); err != nil {
-      return ServerError(`There was a problem updating the subject record.`, err)
-    } else { */
-      qu := db.Model(u).
-        ExcludeColumn(updateExcludes...).
-        Where(`"user".id=?id`)
-      qu.GetModel().Table().SoftDeleteField = nil
-      if _, err := qu.Update(); err != nil {
-        return ServerError(`There was a problem updating the user record.`, err)
-      } else {
-        return nil
-      }
-    // }
-  }
+  return DoRawUpdate(u.UpdateRawQueries(db), db)
 }
 
 // Archive updates a User record in the DB. As Users are logically abstract, one would typically only call this as part of another items archive sequence.
